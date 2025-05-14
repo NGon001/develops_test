b363173fd3051a6c87648b3f3a541838cb4de1cc
NGON <0pixeltech0@gmail.com>
2025-05-14 16:03:56 -0600

    Refactor image URL extraction logic in blog.spec.js for improved readability and efficiency, add comments

--- tests/blog.spec.js
+++ tests/blog.spec.js
@@ -10,29 +10,35 @@ test('Images loading check', async ({ page, request}) => {
     const BlogsURLset = new Set();
     await test.step('Open blogs page and get blogs urls', async step => {
       await page.goto('/blog/tags/latest');
-      const links = await page.locator('a[href^="/blog/"]').all(); // all links (a) with href (a[href]) and starts with /blog/  (a[href^="/blog/"])
-      for (const link of links) {
-          const href = await link.getAttribute('href');
-          if (href && !href.includes('/tags/')) {
-            BlogsURLset.add(href);
-          }
-      }
+
+      const links = await page.locator('a[href^="/blog/"]').evaluateAll(links =>
+        links
+        .map(link => link.getAttribute('href'))
+        .filter(href => href && !href.includes('/tags/'))
+      ); // Get all blogs links
+
+      links.forEach(href => BlogsURLset.add(href)); // filter links to have only unique links
     });
 
     for(let url of BlogsURLset){
       await test.step(`Check images on ${url}`, async () => {
         await page.goto(url);
 
+        //Get all images srcsets
         const srcsets = await page.locator('//picture/source[@srcset]').evaluateAll(sources =>
           sources.map(source => source.getAttribute('srcset'))
         );
 
+        //Filter images to array split from 3x,2x, spaces and ,
         const imageUrls = srcsets.flatMap(srcset =>
           srcset ? srcset.split(',').map(part => part.trim().split(' ')[0]) : []
         );
 
+
+        //Make sure that urls are unique
         const uniqueImageUrls = [...new Set(imageUrls)];
 
+        //Make a requests and save it into object {image link, status (200,403, etc...)}
         const responses = await Promise.all(
           uniqueImageUrls.map(src => request.get(src)
           .then(res => ({ 
@@ -41,6 +47,7 @@ test('Images loading check', async ({ page, request}) => {
             }))
         ));
 
+        //Check if all responses was success (200)
         for (const { src, status } of responses) {
           expect.soft(status, `Image status code ${status} for: ${src} on page ${url}`).toBe(200);
         }
